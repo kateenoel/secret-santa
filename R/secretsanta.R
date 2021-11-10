86f301f792005a2d390fc8aab496bc0b4e1e94c7
Kate Noel <noel0047@umn.edu>
2021-11-10 17:13:42 -0500

    Add roxygen documentation and email communication

--- R/secretsanta.R
+++ R/secretsanta.R
@@ -1,3 +1,12 @@
+#' secretsanta
+#'
+#' WHAT IT DOES
+#' @param sender_email DESCRIPTION
+#' @param sender_pwd DESCRIPTION
+#' @examples
+#' example1
+#' example2
+#' @export
 secretsanta <- function(sender_email, sender_pwd) {
   # determine number of participants
   num_santas <- as.integer(readline(prompt = "How many participants in your secret santa group? "))
@@ -13,25 +22,36 @@ secretsanta <- function(sender_email, sender_pwd) {
   }
 
   # randomly assign secret santas until no one is their own secret santa
-  santa <- sample(names)
+  santa <- names
   santee <- sample(names)
 
-  i <- 1
   while (any(santa == santee)) {
     santee <- sample(names)
-    i <- i + 1
   }
-  assignments <- data.frame(santa,santee)
 
   # prepare variables for email
-  # consisent across recipients
-  sender <- user.name <- sender_email
-  subject <- "secret santa assignment"
-  hostname <- "smtp.gmail.com"
-  port <- 465
-  passwd <- sender_pwd
+  # consistent across recipients
+  sender <- sender_email
+  subj <- "secret santa assignment :)"
+  host <- "smtp.gmail.com"
+  portnum <- 465
+  username <- sender_email
+  pwd <- sender_pwd
 
   # unique to recipient
-
-
+  for (i in 1:num_santas) {
+    recipient <- emails[i]
+    text <- paste("hi ", names[i], ', your secret santee is ', santee[i], ".", ' in other words, you are the secret santa of ', santee[i], ". have fun!", sep="")
+    send.mail(from = sender,
+              to = recipient,
+              subject = subj,
+              body = text,
+              html = T,
+              smtp = list(host.name = host,
+                          port = portnum,
+                          user.name = username,
+                          passwd = pwd,
+                          ssl = T),
+              authenticate = T)
+  }
 }
