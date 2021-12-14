7edfc2f0341e740e7145e6cf7867343d99a12d74
Kate Noel <noel0047@umn.edu>
2021-12-13 21:10:40 -0500

    Store data in list format.
    Add shipping address, wants, not wants, etc

--- R/secretsanta.R
+++ R/secretsanta.R
@@ -11,19 +11,40 @@ secretsanta <- function(sender_email, sender_pwd) {
   # initialize name and email vectors
   names <- c()
   emails <- c()
+  addresses <- c()
+  wants <- c()
+  notwants <- c()
 
   # for each santa, assign name and email
   for (i in 1:num_santas) {
     names <- c(names, assign(paste("santa", i, "_name", sep=""), readline(prompt = paste("What is santa #", i, "'s name? ", sep=""))))
     emails <- c(emails, assign(paste("santa", i, "_email", sep=""), readline(prompt = paste("What is santa #", i, "'s email? ", sep=""))))
+    addresses <- c(addresses, assign(paste("santa", i, "_address", sep=""), readline(prompt = paste("What is santa #", i, "'s shipping address? ", sep=""))))
+    wants <- c(wants, assign(paste("santa", i, "_want", sep=""), readline(prompt = paste("What are santa #", i, "'s wants? ", sep=""))))
+    notwants <- c(notwants, assign(paste("santa", i, "_notwant", sep=""), readline(prompt = paste("What are santa #", i, "'s not wants? ", sep=""))))
+  }
+
+  # for each santa, create a dictionary with all relevant information
+  people <- vector(mode='list', length=num_santas)
+
+  for (i in 1:num_santas) {
+    name = names[i]
+    email = emails[i]
+    address = addresses[i]
+    want = wants[i]
+    notwant = notwants[i]
+
+    person = c("name" = name, "email" = email, "address" = address, "want" = want, "notwant" = notwant)
+
+    people[[i]] <- person
   }
 
   # randomly assign secret santas until no one is their own secret santa
   santa <- names
-  santee <- sample(names)
+  santee <- sample(santa)
 
   while (any(santa == santee)) {
-    santee <- sample(names)
+    santee <- sample(santa)
   }
 
   # prepare variables for email
@@ -38,7 +59,7 @@ secretsanta <- function(sender_email, sender_pwd) {
   # unique to recipient
   for (i in 1:num_santas) {
     recipient <- emails[i]
-    text <- paste("hi ", names[i], ', your secret santee is ', santee[i], ".", ' in other words, you are the secret santa of ', santee[i], ". have fun!", sep="")
+    text <- paste("hi ", santa[i], ', your secret santee is ', santee[i], ".", ' in other words, you are the secret santa of ', santee[i], ". have fun!", sep="")
     send.mail(from = sender,
               to = recipient,
               subject = subj,
